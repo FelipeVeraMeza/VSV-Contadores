cfc66ce21f024e9d59e2c1c12190c8f6dbbb97e6
Felipe <felipe.veram2001@gmail.com>
2026-06-29 14:49:26 -0400

    cambios

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const IP_PUBLICA = 'vsv-contadores-production.up.railway.app';
+const IP_PUBLICA = 'vsv-contadores-production-97af.up.railway.app';
 
 //export const API_BASE_URL = 'http://localhost:4000/api';
 export const API_BASE_URL = `https://${IP_PUBLICA}/api`;
